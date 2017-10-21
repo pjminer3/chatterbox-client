c7c078047e08ee0ca4c1356decf1b79f81dfc56c
pjminer3 <patrickminer318@gmail.com>
2017-10-20 18:51:52 -0700

    Stopped chasing the wrong rabbit. Started chasing the right rabbit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,64 @@
 // YOUR CODE HERE:
 
+var app = {
+  messageArray: [], 
+  
+  init: function() {
+  
+  }, 
+  
+  send: function(message) {
+    // ajax POST request returns object
+    responseObject = $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message, // mess with this later
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+        //console.log('this is data', data);
+        return data;
+      },
+      error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  
+  }, 
+  
+  fetch: function() {
+  
+  },
+  
+  clearMessage: function() {
+  
+  },
+   
+  renderMessage: function() {
+  
+  },
+  
+  renderRoom: function() {
+  
+  },
+  
+  handleUsernameClick: function() {
+  
+  },
+  
+  handleSubmit: function() {
+    
+  }
+};
 
 
 $(document).ready(function() {
   var grossUsername = window.location.search;
   var username = grossUsername.slice(grossUsername.indexOf('=') + 1);
 
-    
+  
     
   $('#messageInput').on('keypress', function(e) {
     // If user 'enters' message, creates message object
@@ -16,21 +68,8 @@ $(document).ready(function() {
         text: $('#messageInput').val(),
         //roomname: '4chan'
       };
-
-      $.ajax({
-        // This is the url you should use to communicate with the parse API server.
-        url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-        type: 'POST',
-        data: JSON.stringify(message), // mess with this later
-        contentType: 'application/json',
-        success: function (data) {
-          console.log('chatterbox: Message sent');
-        },
-        error: function (data) {
-            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-          console.error('chatterbox: Failed to send message', data);
-        }
-      });
+      
+      app.send(message);
         
       // return false -- stops propogation and all that jaz
       return false;
